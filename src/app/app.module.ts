92da439e648eb38eaab745e2809406f814c285c6
Emily Benitez <ebeni7ez@users.noreply.github.com>
2018-08-02 16:43:27 +0200

    Built app skeleton by adding new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemsComponent } from './recipes/recipe-items/recipe-items.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemsComponent,
+    RecipeDetailsComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
